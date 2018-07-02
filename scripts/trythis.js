44968de163e321e37088b07f59760e2f2ede8c2f
nrwebmedia <33914460+nrwebmedia@users.noreply.github.com>
2018-07-02 11:53:59 -0500

    Add files via upload

---
+++ scripts/trythis.js
@@ -0,0 +1,17 @@
+  var userFeed = new Instafeed({
+    get: 'user',
+    userId: '5579681984',
+    clientId: '6c1da4d4e7124243887a055f8d899a59',
+    accessToken: '5579681984.6c1da4d.25216bfea1af4da790cea474c85c16ff',
+    resolution: 'standard_resolution',
+    template: '<a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a>',
+    sortBy: 'most-recent',
+    limit: 4,
+    links: false
+  });
+  userFeed.run();
+  $('a:last').replaceWith(function() {
+  return $('<div/>', {
+    html: $(this).html()
+  }).addClass('makediv');
+});
