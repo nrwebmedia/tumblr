1f83f3a3a3c049298ca74df7045d7b0055d9db74
nrwebmedia <33914460+nrwebmedia@users.noreply.github.com>
2018-07-02 12:36:37 -0500

    Add files via upload

---
+++ scripts/trythis2.js
@@ -0,0 +1,23 @@
+  var userFeed = new Instafeed({
+    get: 'user',
+    userId: '5579681984',
+    clientId: '6c1da4d4e7124243887a055f8d899a59',
+    accessToken: '5579681984.6c1da4d.25216bfea1af4da790cea474c85c16ff',
+    resolution: 'standard_resolution',
+    template: '<a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a>',
+    sortBy: 'most-recent',
+    limit: 4,
+    links: false,
+      filter: function(image) {
+        if (count === 0) {
+            className += "firstimg";
+        }
+        if (count === 4) {
+            className += "lastimg";
+        }
+        image.customClassName = className;
+        count++;
+        return true;
+    }
+  });
+  userFeed.run();
