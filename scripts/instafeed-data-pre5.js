973225be925272957a20dcfd334a5cc192ce13ea
nrwebmedia <33914460+nrwebmedia@users.noreply.github.com>
2018-07-02 14:53:47 -0500

    Add files via upload

---
+++ scripts/instafeed-data-pre5.js
@@ -0,0 +1,16 @@
+  var userFeed = new Instafeed({
+    get: 'user',
+    userId: '5579681984',
+    clientId: '6c1da4d4e7124243887a055f8d899a59',
+    accessToken: '5579681984.6c1da4d.25216bfea1af4da790cea474c85c16ff',
+    resolution: 'standard_resolution',
+    template: '<a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a>',
+      after: function() {
+        var url = $("#instafeed a:last").attr("href", "https://instagram.com/" + "{{model.user.username}}");
+        var url = $("#instafeed a:last").attr("title", "@" + "{{model.user.username}}" + "on Instagram");
+      },
+    sortBy: 'most-recent',
+    limit: 4,
+    links: false
+  });
+  userFeed.run();
